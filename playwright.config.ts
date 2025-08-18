f7c70ce1f088a91269c2ab7efd6ac55115f6a7bc
jacti <jactio99@gmail.com>
2025-08-18 16:37:28 +0900

    Chore : husky setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   /* Start Next.js app before tests. For CI you can switch to build+start. */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run build && npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
     timeout: 120_000,
