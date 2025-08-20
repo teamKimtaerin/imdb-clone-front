d7f9574441a980e981ccb5bf74fc2cf28334d2ea
DongGyu Kim <shfkemll@gmail.com>
2025-08-20 19:11:04 +0900

    Fix: 무한 스크롤 버그 해결

--- src/types/movieCardProps.ts
+++ src/types/movieCardProps.ts
@@ -1,3 +1,5 @@
+import { Director } from './movie';
+
 export interface MovieCardProps {
   _id: string;
   title: string;
@@ -9,9 +11,10 @@ export interface MovieCardProps {
   audience: number;
   trailer_url?: string;
   description: string;
-  director: string;
+  director: Director;
   poster_url?: string;
-  age_rating: string; // 시청 등급: ALL, 12, 15, 18 등
+  age_rating: string; // 시청 등급: ALL, 12, 15, 18, NR 등
+  is_adult_content: boolean; // 18등급 여부 (블러 효과용)
   created_at?: string;
   __v?: number;
   rank?: number;
